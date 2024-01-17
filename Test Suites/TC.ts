f386b4234c7701118e173b47da0d57b7efb3225c
Bhavyansh Ameta <bhavyanshameta@gmail.com>
2024-01-17 21:04:46 +0530

    dsd

--- Test Suites/TC.ts
+++ Test Suites/TC.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>f21bb750-0724-4bac-990f-b2ff34e69781</testSuiteGuid>
+   <testCaseLink>
+      <guid>a8e3fa1e-f81e-410d-a78f-56d13f731eab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
